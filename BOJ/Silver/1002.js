01f23c5abd8f5bce00d81e8e6731edd1460589fa
didgmltmd <gomboy11@naver.com>
2025-11-05 15:35:22 +0900

    feat: 폴더 구조 변경

--- BOJ/Silver/1002.js
+++ BOJ/Silver/1002.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
-const input = fs.readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
-                .toString()
-                .trim()
-                .split("\n")
-                .map(Number);
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
 
 const T = input[0];
 const cases = input.slice(1);
@@ -17,6 +18,6 @@ for (let i = 2; i <= 40; i++) {
   dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
 }
 
-cases.forEach(n => {
+cases.forEach((n) => {
   console.log(dp[n][0], dp[n][1]);
 });
